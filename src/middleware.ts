5030f4708aadcde8ee1d2962789266801016aa2b
adrianns9 <145073675+adrianns9@users.noreply.github.com>
2024-11-23 15:29:43 -0500

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,15 @@ import db from "@/lib/dbServer";
 
 export async function middleware(request: NextRequest) {
   const isLoggedIn = await db.isAuthenticated(request.cookies);
+  const pathname = request.nextUrl.pathname;
 
-  if (request.nextUrl.pathname) {
+  if (pathname && pathname.includes("/auth")) {
     if (isLoggedIn) {
-      // return NextResponse.redirect(new URL("/dashboard", request.url));
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+
+    if (pathname.endsWith("/auth")) {
+      return NextResponse.redirect(new URL("/auth/login", request.url));
     }
   }
 }
